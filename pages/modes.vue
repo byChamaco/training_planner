a1deb23ad1c451d6fb75a5b8fbfc278fe4aea09a
mikisb11 <95701817+mikisb11@users.noreply.github.com>
2024-04-21 19:35:40 +0200

    fix pages

--- pages/modes.vue
+++ pages/modes.vue
@@ -1,1 +1,20 @@
-temp
+<template>
+    <div>
+        <h1>Inicion de sesión</h1>
+        <input type="text">
+    </div>
+</template>
+  
+<script>
+    export default {
+        name: 'List',
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+  
+<style>
+
+</style>
---
+++ pages/training.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Inicion de sesión</h1>
+        <input type="text">
+    </div>
+</template>
+  
+<script>
+    export default {
+        name: 'List',
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+  
+<style>
+
+</style>
